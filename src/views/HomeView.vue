ffdf751e561374e9785eb5fb4af2242af2f751c5
amustapha <amustapha@linkfire.com>
2022-04-29 00:03:56 +0100

    feature/init files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="bg-red-400">Welcome to tailwindcss</h1>
+    <TheWelcome />
+  </main>
+</template>
