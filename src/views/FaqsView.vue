7e813a8f205b8d9831062bba0ef6fcc9c425fd69
Yaasir024 <yope4ever@gmail.com>
2022-04-29 21:38:17 +0100

    feature/ created layout for the Faq page

--- src/views/FaqsView.vue
+++ src/views/FaqsView.vue
@@ -1,3 +1,14 @@
 <template>
-    <h1>This is the Frequently asked question page</h1>
+    <section class="faq mx-140px text-center border-x-2 border-unify-beige border-dashed">
+        <h2 class="text-xl text-unify-purple-300 leading-140 tracking-0.01 font-medium mt-70px ">Have Questions?</h2>
+        <h1 class="text-32px text-unify-dark-300 font-medium tracking-0.01 mt-3">FAQs</h1>
+
+        <section class="accordion mt-102px">
+            <div>
+                <div class="question">
+                    <h3>Is there a web application for Unify?</h3>
+                </div>
+            </div>
+        </section>
+    </section>
 </template>
