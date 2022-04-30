8cb37988073c4e35c3aeb3d212a66bd08b8caae0
salskhal <olaniyisal@gmail.com>
2022-04-30 06:03:31 +0100

    feature/ started working on video thumbnail

--- src/views/Home/Sections/Video.vue
+++ src/views/Home/Sections/Video.vue
@@ -1,5 +1,9 @@
 <template>
-    <main>
-        ada
-    </main>
+  <main>
+    <video controls class="max-w-[915px] max-h-[439px]">
+      <source src="./video.mp4" type="video/mp4" />
+    </video>
+    <div></div>
+    <div></div>
+  </main>
 </template>
