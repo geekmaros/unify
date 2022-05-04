da8a9eb82cda7bff6e85d7b76af2ad5539987d7f
amustapha <amustapha@linkfire.com>
2022-05-04 11:01:40 +0100

    fix/ more fixes on splider and vite config

--- vite.config.js
+++ vite.config.js
@@ -5,36 +5,36 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      name: "VueSplide",
-      entry: "./src/main.js",
-      formats: ["es", "cjs"],
-      fileName: (format) => {
-        let suffix = format;
-
-        if (format === "es") {
-          suffix = "esm";
-        }
-
-        return `js/vue-splide.${suffix}.js`;
-      },
-    },
-    emptyOutDir: false,
-    watch: {
-      include: "src/**",
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
-        },
-        exports: "named",
-      },
-    },
-    minify: false,
-  },
+  // build: {
+  //   lib: {
+  //     name: "VueSplide",
+  //     entry: "./src/main",
+  //     formats: ["es", "cjs"],
+  //     fileName: (format) => {
+  //       let suffix = format;
+  //
+  //       if (format === "es") {
+  //         suffix = "esm";
+  //       }
+  //
+  //       return `js/vue-splide.${suffix}.js`;
+  //     },
+  //   },
+  //   emptyOutDir: false,
+  //   watch: {
+  //     include: "src/**",
+  //   },
+  //   rollupOptions: {
+  //     external: ["vue"],
+  //     output: {
+  //       globals: {
+  //         vue: "Vue",
+  //       },
+  //       exports: "named",
+  //     },
+  //   },
+  //   minify: false,
+  // },
   plugins: [vue()],
   resolve: {
     alias: {
